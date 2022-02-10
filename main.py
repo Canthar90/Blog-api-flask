27eaef2d15bab831d6e1115591facc6d34e8858e
Canthar90 <d.cieslak90@gmail.com>
2022-02-10 18:09:04 +0100

    Adding unseciure forms and contact form unsecured but i will work on that

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from datetime import date
+from smtplib import SMTP
+import os
 
 app = Flask(__name__)
 
@@ -14,8 +16,8 @@ def home():
     posts = response.json()
     # print(posts)
     year = date.today()
-    # print(year)
-    return render_template("index.html", posts=posts, date=year)
+    message = "A random stuff from the internet"
+    return render_template("index.html", posts=posts, date=year, image="/assets/img/home-bg.jpg", message=message)
 
 
 @app.route('/about')
@@ -32,9 +34,30 @@ def post(index):
     return render_template("post.html", post=post, date=year, image=pictiure)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET','POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'GET':
+        return render_template("contact.html")
+    elif request.method == 'POST':
+        name = request.form['contact_name']
+        contact_email = request.form['contact_email']
+        phone_nr = request.form['contact_phone_number']
+        message_tosent = request.form['contact_message']
+        email = os.environ.get('EMAIL')
+        password = os.environ.get('PASSWORD')
+        title = "Message was send succesfully"
+        end_message = f"The {name} want to talk \n\n Name: {name} \n Phone Nr: {phone_nr} \n Adress: {contact_email}\n" \
+                      f"Want's to talk about: \n {message_tosent}"
+        with SMTP("smtp.gmail.com") as smtp:
+            smtp.starttls()
+            smtp.login(user=email, password=password)
+            smtp.sendmail(from_addr=email, to_addrs=email, msg=end_message)
+
+        return render_template('header.html', message=title,image='/assets/img/contact-bg.jpg') + render_template('navigation.html')
+
+
+
+
 
 
 
